3d805b1bebb3f08422725c2f3683edae2aaf90ab
Zack Winchell <zackwinchell@gmail.com>
2021-04-30 21:34:01 -0700

    Add super basic level tiles

--- godot/block/block.gd
+++ godot/block/block.gd
@@ -1,11 +1,10 @@
 extends StaticBody2D
 
-var height: int = 32
-var width: int = 32
+var grid_size: int = 32
+
+export var height: int = 1
+export var width: int = 1
 
 func _draw():
-	var rect = Rect2(Vector2(0, 0), Vector2(width, height))
+	var rect = Rect2(0, 0, grid_size * width, grid_size * height)
 	draw_rect(rect, Color(0.4, 0.4, 0.4))
-
-func _ready():
-	pass
